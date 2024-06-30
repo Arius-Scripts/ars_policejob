d5f93e6e4c79f746dff4ef9f18d1a386b6a7c428
Arius <70983185+Arius-Development@users.noreply.github.com>
2024-06-30 20:36:29 +0200

    fixed typos

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,37 +12,37 @@ version '1.0.4'
 --#--
 
 client_scripts {
-	"modules/utils/client/utils.lua",
+	"modules/utils/client.lua",
 	'client.lua',
 
 	"modules/compatibility/frameworks/**/client.lua",
 
-	'modules/menu/client/main.lua',
-	'modules/menu/client/menus.lua',
+	'modules/job/menu/client/main.lua',
+	'modules/job/menu/client/menus.lua',
 
 
-	'modules/actions/client/clothing.lua',
-	'modules/actions/client/armory.lua',
-	'modules/actions/client/garage.lua',
-	'modules/actions/client/interdict_area.lua',
-	'modules/actions/client/stashes.lua',
-	'modules/actions/client/cameras.lua',
-	'modules/actions/client/bossmenu.lua',
-	'modules/actions/client/duty.lua',
+	'modules/job/actions/client/clothing.lua',
+	'modules/job/actions/client/armory.lua',
+	'modules/job/actions/client/garage.lua',
+	'modules/job/actions/client/interdict_area.lua',
+	'modules/job/actions/client/stashes.lua',
+	'modules/job/actions/client/cameras.lua',
+	'modules/job/actions/client/bossmenu.lua',
+	'modules/job/actions/client/duty.lua',
 
-	'modules/interactions/client.lua',
-	'modules/interactions/drag/client.lua',
-	'modules/interactions/handcuffs/client.lua',
-	'modules/interactions/fine/client.lua',
-	'modules/interactions/escort/client.lua',
-	'modules/interactions/props/client.lua',
-	'modules/items/**/client.lua',
+	'modules/job/interactions/client.lua',
+	'modules/job/interactions/drag/client.lua',
+	'modules/job/interactions/handcuffs/client.lua',
+	'modules/job/interactions/fine/client.lua',
+	'modules/job/interactions/escort/client.lua',
+	'modules/job/interactions/props/client.lua',
+	'modules/job/items/**/client.lua',
 }
 
 server_scripts {
 	'server.lua',
-	'modules/menu/server.lua',
-	'modules/interactions/**/server.lua',
+	'modules/job/menu/server.lua',
+	'modules/job/interactions/**/server.lua',
 	"modules/compatibility/frameworks/**/server.lua",
 }
 
